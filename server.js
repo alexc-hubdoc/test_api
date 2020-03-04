6ebc0e2700fa0a32de5aacd18dd48925e86a62bd
alexc-hubdoc <alex.curci@xero.com>
2020-03-03 23:05:08 -0500

    Initial add of project files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/status', (req, res) => {
+  console.log(req.params);
+  res.send(`Test API is running on port ${port}`);
+});
+
+// localhost:5000/greeting?name=alex
+app.get('/greeting', (req, res) => {
+  if (req.query.name) {
+    res.send(`Hello ${req.query.name}`).status(200);
+  } 
+  res.json({ greeting: 'Why hello there!' }).status(200);
+})
+
+app.post('/test', (req, res) => {
+  console.log('Request Body: ', req.body);
+  res.json({ test_response: 'Hello from test post route' }).status(200);
+})
+
+app.listen(port, () => console.log(`Example API listening on port ${port}!`))
